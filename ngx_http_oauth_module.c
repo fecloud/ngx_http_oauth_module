5d24f96fef34f7a62245fa6e49b2ec45328db2fe
dev <dev@ubuntu.(none)>
2015-10-19 21:42:18 -0700

    release 2.0 add cookie oauth support

--- ngx_http_oauth_module.c
+++ ngx_http_oauth_module.c
@@ -28,6 +28,10 @@ typedef struct {
 static ngx_int_t ngx_http_oauth_handler(ngx_http_request_t *r);
 static ngx_int_t ngx_http_oauth_handler_check(ngx_http_request_t *r,
     ngx_str_t *realm, ngx_str_t *user_file);
+static ngx_int_t ngx_http_oauth_handler_check_querystring(ngx_http_request_t *r,
+    ngx_str_t *realm, ngx_str_t *user_file);
+static ngx_int_t ngx_http_oauth_handler_check_cookie(ngx_http_request_t *r,
+    ngx_str_t *realm, ngx_str_t *user_file);
 static ngx_int_t
     ngx_http_oauth_handler_user_file(ngx_http_request_t *r,
     char *query_value, ngx_str_t *user_file);
@@ -38,47 +42,18 @@ static char *ngx_http_oauth_merge_loc_conf(ngx_conf_t *cf,
 static ngx_int_t ngx_http_oauth_init(ngx_conf_t *cf);
 static char *ngx_http_oauth_user_file(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
-    
 /**
- * 取cookie中指定的name
- */
-static char* get_cookie(ngx_http_request_t *r, char* name)
-{
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "get_cookie cookie len:%d\n", r->headers_in.cookies.nelts);
-    
-    if (&r->headers_in.cookies) {
-        //如果有cookie
-        if (r->headers_in.cookies.nelts) {
-            
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "array = 0x%X\n", r->headers_in.cookies);  
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "  .elts = 0x%X\n", r->headers_in.cookies.elts);  
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "  .nelts = %d\n", r->headers_in.cookies.nelts);  
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "  .size = %d\n", r->headers_in.cookies.size);  
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "  .nalloc = %d\n", r->headers_in.cookies.nalloc);  
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "  .pool = 0x%X\n",r->headers_in.cookies.pool); 
-            
-            ngx_table_elt_t ** cookies = r->headers_in.cookies.elts;  
-            
-            ngx_uint_t i = 0;
-            for (; i < r->headers_in.cookies.nelts; i++) {   
-                ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "Cookie key %s\n", cookies[i]->key.data); 
-                ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "Cookie line %s\n", cookies[i]->value.data);         
-            } 
-        }
-        
-    }
-    return NULL;
-}
-
+ * 以=为分隔符取vlaue
+ */ 
 static char* eq_query_name(ngx_http_request_t *r, char *str, char *name) {
     char *index = ngx_strchr(str, '=');
     if (index != NULL && index != str) {
-        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth found %s\n", "=");
+        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "eq_query_name found %s\n", "=");
         int query_name_len = index - str;
         char* query_name = malloc(query_name_len + 1);
         ngx_memzero(query_name, query_name_len + 1);
         ngx_memcpy(query_name, str, query_name_len);
-        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth query_name:%s\n", query_name);
+        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "eq_query_name query_name:%s\n", query_name);
 
         int query_value_len = (str + ngx_strlen(str)) - index - 1;
         if (query_value_len == 0) {
@@ -87,10 +62,10 @@ static char* eq_query_name(ngx_http_request_t *r, char *str, char *name) {
         char* query_value = malloc(query_value_len + 1);
         ngx_memzero(query_value, query_value_len + 1);
         ngx_memcpy(query_value, index + 1, query_value_len);
-        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth query_value:%s\n", query_value);
+        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "eq_query_name query_value:%s\n", query_value);
 
         int cmp = ngx_strcmp(name, query_name);
-        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth cmp:%d\n", cmp);
+        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "eq_query_name cmp:%d\n", cmp);
         if (cmp == 0) {
             free(query_name);
             query_name = NULL;
@@ -102,39 +77,100 @@ static char* eq_query_name(ngx_http_request_t *r, char *str, char *name) {
             query_value = NULL;
         }
     }
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth eq_query_name result:%d\n", 0);
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "eq_query_name result:%d\n", 0);
+    return NULL;
+}
+
+/**
+ * 取cookie中指定的name
+ */
+static char* get_cookie(ngx_http_request_t *r, char* name)
+{
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "get_cookie cookie len:%d\n", r->headers_in.cookies.nelts);
+    
+    if (&r->headers_in.cookies) {
+        //如果有cookie
+        if (r->headers_in.cookies.nelts) {
+            
+            ngx_table_elt_t ** cookies = r->headers_in.cookies.elts;  
+            
+            ngx_uint_t i = 0;
+            for (; i < r->headers_in.cookies.nelts; i++) {   
+                ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "get_cookie Cookie key %s\n", cookies[i]->key.data);
+
+                if (ngx_strcasecmp((u_char*)"Cookie", (u_char*)cookies[i]->key.data) == 0) {
+                    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "get_cookie Cookie line %s\n", cookies[i]->value.data);
+                    
+                    u_char *cookie_data = cookies[i]->value.data;
+                    //如果cookie只有一对key value
+                    if (ngx_strchr(cookie_data, ';') == NULL) {
+                        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "get_cookie only one query:%s\n", cookie_data);
+                        return eq_query_name(r, (char*)cookie_data, name);
+                    } else {
+						
+						//复制querystring
+						int cookie_line_len = cookies[i]->value.len + 1;
+						char *cookie_line_str = (char*)malloc(cookie_line_len);
+						ngx_memzero(cookie_line_str, cookie_line_len);
+						ngx_memcpy(cookie_line_str, cookie_data, cookie_line_len -1 );
+						
+                        const char *token = "; ";
+                        char *pch;
+                        char *result = NULL;
+
+                        for (pch = strtok (cookie_line_str, token); pch != NULL; pch = strtok (NULL, token)) {
+                            result = eq_query_name(r, pch, name);
+                            if (result != NULL) {
+                                break;
+                            }
+                        }
+						
+						free(cookie_line_str);
+						cookie_line_str = NULL;
+						
+                        return result;
+                    }
+                } 
+                   
+            } 
+        }
+        
+    }
     return NULL;
 }
 
 /**
  * 取querystring
  */
-static char* get_querystring(ngx_http_request_t *r, char *str, char* name)
+static char* get_querystring(ngx_http_request_t *r, char* name)
 {
- 
-    if (str == NULL || name == NULL) {
+    if (name == NULL || r->args.len == 0) {
         return NULL;
     }
+	
+	//复制querystring
+	int query_str_len = r->args.len + 1;
+	char *query_str = (char*)malloc(query_str_len);
+	ngx_memzero(query_str, query_str_len);
+	ngx_memcpy(query_str, r->args.data, r->args.len);
 
-    int str_len = ngx_strlen(str);
-    if (str_len == 0) {
-        return NULL;
-    }
-
-    if (ngx_strchr(str, '&') == NULL) {
-        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth only one query:%s\n", str);
-        return eq_query_name(r, str, name);
+    if (ngx_strchr(query_str, '&') == NULL) {
+        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth only one query:%s\n", query_str);
+        return eq_query_name(r, query_str, name);
     }
 
     char * pch;
     char *result = NULL;
-    for (pch = strtok (str,"&"); pch != NULL; pch = strtok (NULL, "&")) {
+    for (pch = strtok (query_str, "&"); pch != NULL; pch = strtok (NULL, "&")) {
         result = eq_query_name(r, pch, name);
         if (result != NULL) {
             break;
         }
     }
     
+	free(query_str);
+	query_str = NULL;
+	
     return result;
 }
 
@@ -225,44 +261,61 @@ ngx_http_oauth_handler_check(ngx_http_request_t *r, ngx_str_t *realm, ngx_str_t 
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth_handler_check realm:%s\n", realm->data);
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth_handler_check user_file:%s\n", user_file->data);
 
-    get_cookie(r, "oauth");
+    ngx_int_t http_code = ngx_http_oauth_handler_check_cookie(r, realm, user_file);
 
+    if (http_code != NGX_OK) {
+        http_code = ngx_http_oauth_handler_check_querystring(r, realm, user_file);
+    }
+
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth_handler_check result:%d\n", http_code);
+    return http_code;
+}
+
+static ngx_int_t 
+ngx_http_oauth_handler_check_querystring(ngx_http_request_t *r, ngx_str_t *realm, ngx_str_t *user_file)
+{
     ngx_int_t http_code = NGX_HTTP_UNAUTHORIZED;
     //有query string
     if (r->args.len) {
 
-        int realm_str_len = realm->len + 1;
-        char *realm_str = (char*)malloc(realm_str_len);
-        ngx_memzero(realm_str, realm_str_len);
-        ngx_memcpy(realm_str, realm->data, realm->len);
-        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth realm_str:%s\n", realm_str);
+        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth_handler_check_querystring request args:%s\n", r->args.data);
 
-        int query_str_len = r->args.len + 1;
-        char *query_str = (char*)malloc(query_str_len);
-        ngx_memzero(query_str, query_str_len);
-        ngx_memcpy(query_str, r->args.data, r->args.len);
-
-        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth request args:%s\n", query_str);
-
-        char *oauth = get_querystring(r, query_str, realm_str);
+        char *oauth = get_querystring(r, (char*)realm->data);
         if (oauth != NULL) {
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth result oauth:%s\n", oauth);
+            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth_handler_check_querystring get_querystring result oauth:%s\n", oauth);
 
             http_code = ngx_http_oauth_handler_user_file(r, oauth, user_file);      
 
             free(oauth);
             oauth = NULL;
         }
-		
-		free(realm_str);
-		realm_str = NULL;
-		free(query_str);
-		query_str = NULL;
     }
-
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth ngx_http_oauth_set_realm result:%d\n", http_code);
+	
     return http_code;
 }
+
+static ngx_int_t 
+ngx_http_oauth_handler_check_cookie(ngx_http_request_t *r, ngx_str_t *realm, ngx_str_t *user_file)
+{
+    ngx_int_t http_code = NGX_HTTP_UNAUTHORIZED;
+
+    if (&r->headers_in.cookies) {
+        //如果有cookie
+        if (r->headers_in.cookies.nelts) {
+            char *oauth = get_cookie(r, (char*)realm->data);
+            if (oauth != NULL) {
+                ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "ngx_http_oauth_handler_check_cookie get_cookie result oauth:%s\n", oauth);
+
+                http_code = ngx_http_oauth_handler_user_file(r, oauth, user_file);      
+
+                free(oauth);
+                oauth = NULL;
+            }
+        }
+    }
+
+    return http_code;
+}    
 
 static ngx_int_t 
 ngx_http_oauth_handler_user_file(ngx_http_request_t *r,
